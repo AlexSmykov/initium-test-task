ad51e7d513c1bac7a6ff3e4c5950c10694fe0213
Alexey <smykovad@cifra-k.ru>
2024-02-21 23:08:09 +0700

    feat: модалки подключены к таблице

--- src/app/pages/main-page/components/user-table/user-table.component.ts
+++ src/app/pages/main-page/components/user-table/user-table.component.ts
@@ -1,21 +1,63 @@
 import { Component, OnInit } from '@angular/core'
-import { TUserTableConfigs } from 'src/app/pages/main-page/components/user-table/user-table.config'
+import {
+  UserTableConfigs,
+  UserTableCreateModalConfig,
+  UserTableDeleteModalConfig,
+} from 'src/app/pages/main-page/components/user-table/user-table.config'
 import { UserTableService } from 'src/app/pages/main-page/components/user-table/user-table.service'
+import { TUser } from 'src/app/core/api/users/users.interface'
+import { UserFormModalHelperService } from 'src/app/pages/main-page/components/user-table/components/user-form/user-form-modal-helper.service'
+import { UserDeleteModalHelperService } from 'src/app/pages/main-page/components/user-table/components/user-delete-modal/user-delete-modal-helper.service'
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.scss'],
-  providers: [UserTableService],
+  providers: [
+    UserTableService,
+    UserFormModalHelperService,
+    UserDeleteModalHelperService,
+  ],
 })
 export class UserTableComponent implements OnInit {
   users = this.userTableService.users$
 
-  configs = TUserTableConfigs
+  tableConfigs = UserTableConfigs
 
-  constructor(private userTableService: UserTableService) {}
+  createModalConfig = UserTableCreateModalConfig
+  deleteModalConfig = UserTableDeleteModalConfig
+
+  isCreateModalShow = false
+  isDeleteModalShow = false
+
+  constructor(
+    private userTableService: UserTableService,
+    private userFormHelperService: UserFormModalHelperService,
+    private userDeleteModalHelperService: UserDeleteModalHelperService
+  ) {}
 
   ngOnInit(): void {
     this.userTableService.updateUsers()
+  }
+
+  onItemClicked(user: TUser): void {
+    this.userFormHelperService.setUser(user)
+    this.isCreateModalShow = true
+  }
+
+  onItemsDelete(users: number[]): void {
+    console.log(users)
+    this.userDeleteModalHelperService.setUsers(users)
+    this.isDeleteModalShow = true
+  }
+
+  closeCreateModal(): void {
+    this.isCreateModalShow = false
+    this.userFormHelperService.resetUser()
+  }
+
+  closeDeleteModal(): void {
+    this.isDeleteModalShow = false
+    this.userDeleteModalHelperService.resetUser()
   }
 }
